07a7864439632cad49e8f15ce0e5c864777c1dac
tom-gough <tom@tgsd.co.uk>
2019-10-17 09:42:54 +0100

    Added endpoint for report trainign provider

---
+++ src/SFA.DAS.ProviderRelationships.Api/RouteValues/Email/SendReportTrainingProviderEmailRouteValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SFA.DAS.ProviderRelationships.Api.RouteValues.Email
+{
+    public class SendReportTrainingProviderEmailRouteValues
+    {
+        public string EmployerEmailAddress { get; set; }
+        public DateTime EmailReported { get; set; }
+        public string TrainingProvider { get; set; }
+        public string TrainingProviderSenderName { get; set; }
+        public DateTime EmailSent { get; set; }
+    }
+}
---
+++ src/SFA.DAS.ProviderRelationships/Application/Commands/SendReportTrainingProviderNotification/SendReportTrainingProviderNotificationCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using SFA.DAS.Notifications.Api.Client;
+using SFA.DAS.Notifications.Api.Types;
+
+namespace SFA.DAS.ProviderRelationships.Application.Commands.SendReportTrainingProviderNotification
+{
+    public class SendReportTrainingProviderNotificationCommandHandler : RequestHandler<SendReportTrainingProviderNotificationCommand>
+    {
+        private readonly INotificationsApi _notificationsApi;
+
+        private const string ReplyToAddress = "noreply@sfa.gov.uk";
+        private const string DefaultSubject = "x";
+        private const string DefaultSystemId = "x";
+        private const string ReportTrainingProviderAddress = "monitoring@esfa.zendesk.com"; //todo config?
+        private const string TemplateId = "ReportTrainingProviderNotification_dev"; //todo when this story is complete _dev should be removed and the associated non dev template created on gov notify
+
+        private const string EmployerEmailAddressTokenKey = "employer_email_address";
+        private const string EmailReportedTokenKey = "email_reported";
+        private const string TrainingProviderTokenKey = "training_provider";
+        private const string TrainingProviderSenderNameTokenKey = "training_provider_name";
+        private const string EmailSentTokenKey = "email_sent";
+
+        public SendReportTrainingProviderNotificationCommandHandler(INotificationsApi notificationsApi)
+        {
+            _notificationsApi = notificationsApi;
+        }
+
+        protected override void Handle(SendReportTrainingProviderNotificationCommand request)
+        {
+            _notificationsApi.SendEmail(new Email(
+                systemId: DefaultSystemId,
+                templateId: TemplateId,
+                subject: DefaultSubject,
+                recipientsAddress: ReportTrainingProviderAddress,
+                replyToAddress: ReplyToAddress,
+                tokens: new Dictionary<string, string> {
+                    { EmployerEmailAddressTokenKey, request.EmployerEmailAddress },
+                    { EmailReportedTokenKey, request.EmailReported.ToString() }, //todo format datetimes correctly following confirmation on story
+                    { TrainingProviderTokenKey, request.TrainingProvider },
+                    { TrainingProviderSenderNameTokenKey, request.TrainingProviderSenderName },
+                    { EmailSentTokenKey, request.EmailSent.ToString() } //todo format datetimes correctly following confirmation on story
+                }
+            ));
+        }
+    }
+}
